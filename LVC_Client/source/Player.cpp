29bc644b12d868afd8914a97ee9d773c23b62c29
Артём <58831226+Letinemeso@users.noreply.github.com>
2026-01-10 16:54:36 +0300

    removed comment

--- LVC_Client/source/Player.cpp
+++ LVC_Client/source/Player.cpp
@@ -28,35 +28,6 @@ Player::~Player()
 
 
 
-// void Player::M_on_sound_end()
-// {
-//     if(!m_sound.sound_data())
-//         return;
-
-//     if(m_sound.is_playing())
-//         return;
-
-//     m_sound.set_sound_data(nullptr);
-//     delete m_sound_data;
-//     m_sound_data = nullptr;
-// }
-
-// void Player::M_start_next_sound_if_needed()
-// {
-//     if(m_sound.sound_data())
-//         return;
-
-//     LSound::Sound_Data* next_sound_data = m_packages_queue->extract_top_data();
-//     if(!next_sound_data)
-//         return;
-
-//     m_sound_data = next_sound_data;
-//     m_sound.set_sound_data(next_sound_data);
-//     m_sound.play();
-// }
-
-
-
 void Player::add_data(const LNet::Package& _voice_package)
 {
     Package_Header header = _voice_package.parse_header<Package_Header>();
